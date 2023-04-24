6f9bdccb128d2c9aa8cf3f507b589f09f3fe5450
Jacob Knowlton <knowlton.jacob17@gmail.com>
2023-04-24 12:52:18 -0600

    Replaced xml layout with jetpack compose

--- app/src/main/java/com/example/example35/ui/MainActivity.kt
+++ app/src/main/java/com/example/example35/ui/MainActivity.kt
@@ -6,25 +6,28 @@ import android.widget.TextView
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.example35.*
-import com.example.example35.database.weather.WeatherData
 import com.example.example35.database.weather.WeatherTable
 import com.example.example35.repository.WeatherViewModel
 import com.example.example35.repository.WeatherViewModelFactory
+import com.example.example35.utils.JSONWeatherUtils
 import kotlin.math.roundToInt
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var mEtLocation: EditText? = null
-    private var mTvTemp: TextView? = null
-    private var mTvPress: TextView? = null
-    private var mTvHum: TextView? = null
-    private var mBtSubmit: Button? = null
-    private var mRecyclerView: RecyclerView? = null
-    private var mAdapter: RecyclerView.Adapter<*>? = null
+class MainActivity : AppCompatActivity() {
 
     // Initialize the view model here. One per activity.
     // While initializing, we'll also inject the repository.
@@ -36,59 +39,64 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //Get the edit text, all the text views, button
-        mEtLocation = findViewById<View>(R.id.et_location) as EditText
-        mTvTemp = findViewById<View>(R.id.tv_temp) as TextView
-        mTvPress = findViewById<View>(R.id.tv_pressure) as TextView
-        mTvHum = findViewById<View>(R.id.tv_humidity) as TextView
-        mBtSubmit = findViewById<View>(R.id.button_submit) as Button
-        mBtSubmit!!.setOnClickListener(this)
-
-        //Set the observer for the vanilla livedata object
-        mWeatherViewModel.data.observe(this, liveDataObserver)
-
-        //set another observer for the flow-converted-to-livedata object
-        // cannot attach observers directly to flows, but the flow
-        // has already been converted to a livedata object
-        mWeatherViewModel.allCityWeather.observe(this, flowObserver)
-
-        //Get the recycler view.
-        mRecyclerView = findViewById<View>(R.id.rv_Master) as RecyclerView
-
-        //Tell Android that we know the size of the recyclerview
-        //doesn't change
-        mRecyclerView!!.setHasFixedSize(true)
-
-        //Set the layout manager
-        val layoutManager = LinearLayoutManager(this)
-        mRecyclerView!!.layoutManager = layoutManager
+        setContent {
+            MainActivityScreen()
+        }
     }
 
-    //create an observer that watches the LiveData<WeatherData> object
-    private val liveDataObserver: Observer<WeatherData> =
-        Observer { weatherData -> // Update the UI if this data variable changes
-            mTvTemp!!.text = "" + (weatherData.temperature.temp - 273.15).roundToInt() + " C"
-            mTvHum!!.text = "" + weatherData.currentCondition.humidity + "%"
-            mTvPress!!.text = "" + weatherData.currentCondition.pressure + " hPa"
-        }
-
-    // This observer is triggered when the Flow object in the repository
-    // detects a change to the database (including at the start of the app)
-    private val flowObserver: Observer<List<WeatherTable>> =
-        Observer { weatherTableList ->
-            mRecyclerView!!.adapter = WeatherRVAdapter(weatherTableList)
-        }
-
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.button_submit -> {
-
-                //Get the string from the edit text and sanitize the input
-                val inputFromEt = mEtLocation!!.text.toString().replace(' ', '&')
-                loadWeatherData(inputFromEt)
+    @Composable
+    fun MainActivityScreen() {
+        var location by remember { mutableStateOf(TextFieldValue()) }
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Enter location:")
+                OutlinedTextField(
+                    value = location,
+                    onValueChange = { location = it },
+                    modifier = Modifier.weight(1f)
+                )
+                Button(onClick = {
+                    val tempLocation = location.text.replace(' ', '&')
+                    loadWeatherData(tempLocation)
+                }) {
+                    Text(text = "Submit")
+                }
             }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            val currentWeather by mWeatherViewModel.data.observeAsState()
+            if (currentWeather != null) {
+                Text(text = "Temperature: " + (currentWeather!!.temperature.temp - 273.15).roundToInt() + " C")
+                Text(text = "Pressure: " + currentWeather!!.currentCondition.pressure + " hPa")
+                Text(text = "Humidity: " + currentWeather!!.currentCondition.humidity + "%")
+                Text(text = "Wind speed: " + currentWeather!!.wind.speed + "m/s")
+                Text(text = "Wind degrees: " + currentWeather!!.wind.deg)
+            }
+
+            val weatherTableList by mWeatherViewModel.allCityWeather.observeAsState()
+            if (weatherTableList != null) {
+                LazyColumn(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    items(weatherTableList!!) { item ->
+                        WeatherListItem(weatherItem = item)
+                    }
+                }
+            }
+
+        }
+    }
+
+    @Composable
+    fun WeatherListItem(weatherItem: WeatherTable) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Text(text = weatherItem.location)
+            Text(text = "" + (JSONWeatherUtils.getWeatherData(weatherItem.weatherJson).temperature.temp - 273.15).roundToInt() + "C")
         }
     }
 
